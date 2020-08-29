8c543fafe00a18b5ba592b7df2e96dcd3ff7e1de
madhumithaa <madhumithaa1411@gmail.com>
2020-08-29 20:57:42 +0530

    doone

--- src/MajorComponents/Body.js
+++ src/MajorComponents/Body.js
@@ -8,47 +8,37 @@ export default class Body extends Component {
       { title: "second", description: "second2" },
     ],
   };
-  render() {
+  componentDidMount() {
     axios
       .get("https://run.mocky.io/v3/0da87c76-9088-45f3-82b1-a9df5d1077d9")
       .then((response) => {
-        // alert("success");
-        // console.log(response.data);
-        // console.log(response.data[0].title);
-        {
-          response.data.map((data) => {
-            let d = {
-              title: data.title,
-              description: data.description,
-            };
-          });
-        }
+        this.setState({
+          ...this.state,
+          data: response.data,
+        });
       })
       .catch((error) => {
         alert("error");
         console.log(error);
       });
+  }
 
+  render() {
     return (
       <div class="container">
         <h5 className="text-muted mb-3">Resources</h5>
         <div className="row">
-          <div className="col-lg-12">
-            {this.state.data.map((data) => {
-              return (
+          {this.state.data.map((data) => {
+            return (
+              <div className="col-lg-6">
                 <Hcard
                   title={data.title}
                   description={data.description}
                   shadow={true}
                 />
-              );
-            })}
-          </div>
-          {/* <div className="col-lg-6">
-            <Hcard />
-            <Hcard />
-            <Hcard />
-          </div> */}
+              </div>
+            );
+          })}
         </div>
       </div>
     );
